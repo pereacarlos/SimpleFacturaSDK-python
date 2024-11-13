969f1c1115561c931bfb2726650de62253bef037
carlosperea14 <carlos.perea@inacapmail.cl>
2024-11-13 12:44:53 -0300

    se hizo cambios

--- SimpleFacturaSDK/main.py
+++ SimpleFacturaSDK/main.py
@@ -3,12 +3,10 @@ from SimpleFacturaSDK.Base import APIClient
 import base64
 import requests
 from SimpleFacturaSDK.models.ResponseDTE import Response
-from SimpleFacturaSDK.models.BoletaHonorarios.BHERequest import BHERequest
-from SimpleFacturaSDK.models.BoletaHonorarios.ListaBHERequest import ListaBHERequest
-import json
-#from requests.auth import HTTPBasicAuth
 from SimpleFacturaSDK.models.GetFactura.Credenciales import Credenciales
-from SimpleFacturaSDK.models.Sucursal import Sucursal
+from SimpleFacturaSDK.models.GetFactura.DteReferenciadoExterno import DteReferenciadoExterno
+from SimpleFacturaSDK.models.GetFactura.SolicitudPdfDte import SolicitudPdfDte
+import json
 from datetime import datetime
 fecha_desde = datetime.strptime("2024-09-03", "%Y-%m-%d").isoformat()
 fecha_hasta = datetime.strptime("2024-11-11", "%Y-%m-%d").isoformat()
@@ -16,25 +14,23 @@ username = "demo@chilesystems.com"
 password = "Rv8Il4eV"
 client_api = APIClient(username, password)
 
-solicitud= ListaBHERequest(
+solicitud= SolicitudPdfDte(
     credenciales=Credenciales(
-        rut_emisor="76269769-6",
-        nombre_sucursal="Casa Matriz"
+        rut_emisor="76269769-6"
     ),
-    Folio=None,
-    Desde=fecha_desde,
-    Hasta=fecha_hasta
+    dte_referenciado_externo=DteReferenciadoExterno(
+        folio=2963,
+        codigo_tipo_dte=33,
+        ambiente=0
+    )
 )
-try:
-    ListadoBHERecibido = client_api.BoletaHonorarioService.ListadoBHERecibido(solicitud)
-    print("\nDatos de la Respuesta:")
-    print(f"Status: {ListadoBHERecibido.status}")
-    print(f"Message: {ListadoBHERecibido.message}")
 
-    for cliente in ListadoBHERecibido.data:
-        print(f"fOLIO: {cliente.folio}")
-        print(f"FECHAEMISION: {cliente.fechaEmision}")
-        print("\n")     
+try:
+    pdf = client_api.Facturacion.obtener_pdf(solicitud)
+    # Guardar PDF
+    with open("factura.pdf", "wb") as file:
+        file.write(pdf)
+    print("PDF guardado exitosamente", pdf)
 
 
 except requests.exceptions.HTTPError as http_err:
