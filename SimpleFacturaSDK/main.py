02a132c9ff1cf75ea991f93341c679deb87a41c9
carlosperea14 <carlos.perea@inacapmail.cl>
2024-11-17 19:43:15 -0300

    se estan realizando cambios

--- SimpleFacturaSDK/main.py
+++ SimpleFacturaSDK/main.py
@@ -1,37 +1,22 @@
-
 from ClientSimpleFactura import ClientSimpleFactura
 import base64
 import requests
 from models.ResponseDTE import Response
 from models.GetFactura.Credenciales import Credenciales
-from models.GetFactura.DteReferenciadoExterno import DteReferenciadoExterno
-from models.GetFactura.SolicitudPdfDte import SolicitudPdfDte
-from enumeracion.TipoDTE import DTEType
-import json
-from datetime import datetime
-fecha_desde = datetime.strptime("2024-09-03", "%Y-%m-%d").isoformat()
-fecha_hasta = datetime.strptime("2024-11-11", "%Y-%m-%d").isoformat()
 username = "demo@chilesystems.com"
 password = "Rv8Il4eV"
 client_api = ClientSimpleFactura(username, password)
-solicitud= SolicitudPdfDte(
-    credenciales=Credenciales(
-        rut_emisor="76269769-6",
-        nombre_sucursal="Casa Matriz"
-    ),
-    dte_referenciado_externo=DteReferenciadoExterno(
-        folio=4117,
-        codigoTipoDte=33,
-        ambiente=0
-    )
-)
+solicitud= Credenciales(rut_emisor="76269769-6")
 try:
-    # Guardar PDF
-    timbre_response = client_api.Facturacion.obtener_timbre(solicitud)
-    timbre_data = json.loads(timbre_response.data)
-    with open("timbre.png", "wb") as f:
-        f.write(base64.b64decode(timbre_data["data"]))
-
+    ListSucursal = client_api.Sucursales.ListarSucursales(solicitud)
+    print("\nDatos de la Respuesta:")
+    print(f"Status: {ListSucursal.status}")
+    print(f"Message: {ListSucursal.message}")
+    for cliente in ListSucursal.data:
+        print(f"Nombre: {cliente.nombre}")
+        print(f"Direccion: {cliente.direccion}")
+        print("\n")
+   
 except requests.exceptions.HTTPError as http_err:
     print(f"Error HTTP: {http_err}")
     print("Detalle del error:", http_err.response.text)
