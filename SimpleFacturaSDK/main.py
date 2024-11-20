cf90ef973bfd945853ee8e76ee241d466590c253
carlosperea14 <carlos.perea@inacapmail.cl>
2024-11-20 16:07:35 -0300

    se refactorizo producto

--- SimpleFacturaSDK/main.py
+++ SimpleFacturaSDK/main.py
@@ -1,24 +1,36 @@
 import asyncio
 from ClientSimpleFactura import ClientSimpleFactura
+from models.ResponseDTE import Response
+import json
 from models.GetFactura.Credenciales import Credenciales
 async def main():
     username = "demo@chilesystems.com"
     password = "Rv8Il4eV"
     client_api = ClientSimpleFactura(username, password)
-    credenciales = Credenciales(
+    solicitud= Credenciales(
         rut_emisor="76269769-6",
         nombre_sucursal="Casa Matriz"
     )
-    path_csv = r"C:\Users\perea\Downloads\ejemplo_carga_masiva_nacional.csv"
+
+
     try:
-        factura = await client_api.Facturacion.facturacion_Masiva(credenciales, path_csv)
+        ListProduct = await client_api.Productos.listarProductos(solicitud)
         print("\nDatos de la Respuesta:")
-        print(factura.data)
-        print("Status Code:", factura.status)
-        print("Message:", factura.message)
+        print(f"Status: {ListProduct.status}")
+        print(f"Message: {ListProduct.message}")
+        for i in ListProduct.data:
+            print(f"productoId: {i.productoId}")
+            print(f"nombre: {i.nombre}")
+            print(f"precio: {i.precio}")
+            print(f"exento: {i.exento}")
+            for imp in i.impuestos:
+                print(f"codigoSii: {imp.codigoSii}")
+                print(f"nombreImp: {imp.nombreImp}")
+                print(f"tasa: {imp.tasa}")
     except Exception as err:
         print(f"Error: {err}")
     finally:
-        await client_api.Facturacion.close()
+        await client_api.Productos.close()
 if __name__ == "__main__":
     asyncio.run(main())
+    
--- SimpleFacturaSDK/services/ProductoService.py
+++ SimpleFacturaSDK/services/ProductoService.py
@@ -2,40 +2,58 @@ from typing import List
 from models.Productos.NuevoProductoExternoRequest import NuevoProductoExternoRequest, ProductoExternoEnt
 from models.Productos.ProductoEnt import ProductoEnt
 from models.ResponseDTE import Response
-import requests
 from Utilidades.Simplificar_error import simplificar_errores
 from models.SerializarJson import serializar_solicitud, serializar_solicitud_dict,dataclass_to_dict
-
+import aiohttp
 
 class ProductoService:
-    def __init__(self, session, base_url):
-        self.session = session
+    def __init__(self, base_url, headers):
         self.base_url = base_url
+        self.headers = headers
+        self.session = aiohttp.ClientSession(headers=self.headers)
 
-    def CrearProducto(self, solicitud) -> Response[List[ProductoEnt]]:
+    async def CrearProducto(self, solicitud) -> Response[List[ProductoEnt]]:
         url = f"{self.base_url}/addProducts"
         solicitud_dict = serializar_solicitud_dict(solicitud)
-        response = self.session.post(url, json=solicitud_dict)
-        contenidoRespuesta = response.text
-        if response.status_code == 200:
-            deserialized_response = Response[List[ProductoEnt]].parse_raw(contenidoRespuesta)
-            return Response(status=200, data=deserialized_response.data)
-        return Response(
-            status=response.status_code,
-            message=simplificar_errores(contenidoRespuesta),
-            data=None
-        )
+        try:
+            async with self.session.post(url, json=solicitud_dict) as response:
+                contenidoRespuesta = await response.text()
+                if response.status == 200:
+                    deserialized_response = Response[List[ProductoEnt]].parse_raw(contenidoRespuesta)
+                    return Response(status=200, data=deserialized_response.data)
+                return Response(
+                    status=response.status,
+                    message=simplificar_errores(contenidoRespuesta),
+                    data=None
+                )
+        except aiohttp.ClientError as err:
+            return Response(
+                status=500,
+                message=f"Error en la conexión: {err}",
+                data=None
+            )
 
-    def listarProductos(self, solicitud) -> Response[List[ProductoExternoEnt]]:
+        
+    async def listarProductos(self, solicitud) -> Response[List[ProductoExternoEnt]]:
         url = f"{self.base_url}/products"
         solicitud_dict = solicitud.to_dict()
-        response = self.session.post(url, json=solicitud_dict)
-        contenidoRespuesta = response.text
-        if response.status_code == 200:
-            deserialized_response = Response[List[ProductoExternoEnt]].parse_raw(contenidoRespuesta)
-            return Response(status=200, data=deserialized_response.data)
-        return Response(
-            status=response.status_code,
-            message=simplificar_errores(contenidoRespuesta),
-            data=None
-        )
+        try:
+            async with self.session.post(url, json=solicitud_dict) as response:
+                contenidoRespuesta = await response.text()
+                if response.status == 200:
+                    deserialized_response = Response[List[ProductoExternoEnt]].parse_raw(contenidoRespuesta)
+                    return Response(status=200, data=deserialized_response.data)
+                return Response(
+                    status=response.status,
+                    message=simplificar_errores(contenidoRespuesta),
+                    data=None
+                )
+        except aiohttp.ClientError as err:
+            return Response(
+                status=500,
+                message=f"Error en la conexión: {err}",
+                data=None
+            )
+
+    async def close(self):
+        await self.session.close()
