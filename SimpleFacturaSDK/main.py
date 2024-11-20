d6784492595c5c84c093305971dd97dd161761d9
carlosperea14 <carlos.perea@inacapmail.cl>
2024-11-20 16:53:37 -0300

    se hizo cambios en proveedor

--- SimpleFacturaSDK/main.py
+++ SimpleFacturaSDK/main.py
@@ -1,36 +1,94 @@
 import asyncio
+
 from ClientSimpleFactura import ClientSimpleFactura
-from models.ResponseDTE import Response
+import base64
+import requests
 import json
+from models.ResponseDTE import Response
+from enumeracion.Ambiente import AmbienteEnum
+from enumeracion.TipoDTE import DTEType
 from models.GetFactura.Credenciales import Credenciales
+from models.Productos.DatoExternoRequest import DatoExternoRequest
+from models.Productos.NuevoProductoExternoRequest import NuevoProductoExternoRequest
+from models.Clientes.NuevoReceptorExternoRequest import NuevoReceptorExternoRequest
+
 async def main():
     username = "demo@chilesystems.com"
     password = "Rv8Il4eV"
     client_api = ClientSimpleFactura(username, password)
-    solicitud= Credenciales(
-        rut_emisor="76269769-6",
-        nombre_sucursal="Casa Matriz"
+    solicitud= DatoExternoRequest(
+        Credenciales=Credenciales(
+            rut_emisor="76269769-6",
+            nombre_sucursal="Casa Matriz"
+        ),
+        Clientes=[
+            NuevoReceptorExternoRequest(
+                Rut="57681892-0",
+                RazonSocial="Cliente Test 1",
+                Giro="Giro 1",
+                DirPart="direccion 1",
+                DirFact="direccion 1",
+                CorreoPar="correo 1",
+                CorreoFact="correo 1",
+                Ciudad="Ciudad 1",
+                Comuna="Comuna 1"
+            ),
+            NuevoReceptorExternoRequest(
+                Rut="56516677-8",
+                RazonSocial="Cliente Test 2",
+                Giro="Giro 2",
+                DirPart="direccion 2",
+                DirFact="direccion 2",
+                CorreoPar="correo 2",
+                CorreoFact="correo 2",
+                Ciudad="Ciudad 2",
+                Comuna="Comuna 2"
+            ),
+            NuevoReceptorExternoRequest(
+                Rut="68959276-7",
+                RazonSocial="Cliente Test 3",
+                Giro="Giro 3",
+                DirPart="direccion 3",
+                DirFact="direccion 3",
+                CorreoPar="correo 3",
+                CorreoFact="correo 3",
+                Ciudad="Ciudad 3",
+                Comuna="Comuna 3"
+            )
+        ]
     )
 
-
     try:
-        ListProduct = await client_api.Productos.listarProductos(solicitud)
+        AddClient = await client_api.Clientes.CrearClientes(solicitud)
         print("\nDatos de la Respuesta:")
-        print(f"Status: {ListProduct.status}")
-        print(f"Message: {ListProduct.message}")
-        for i in ListProduct.data:
-            print(f"productoId: {i.productoId}")
-            print(f"nombre: {i.nombre}")
-            print(f"precio: {i.precio}")
-            print(f"exento: {i.exento}")
-            for imp in i.impuestos:
-                print(f"codigoSii: {imp.codigoSii}")
-                print(f"nombreImp: {imp.nombreImp}")
-                print(f"tasa: {imp.tasa}")
+        print(f"Status: {AddClient.status}")
+        print(f"Message: {AddClient.message}")
+        
+        for cliente in AddClient.data:
+            print(f"ReceptorId: {cliente.receptorId}")
+            print(f"EmisorId: {cliente.emisorId}")
+            print(f"RUT: {cliente.rut}")
+            print(f"Dv: {cliente.dv}")
+            print(f"RutFormateado: {cliente.rutFormateado}")
+            print(f"RazonSocial: {cliente.razonSocial}")
+            print(f"NombreFantasia: {cliente.nombreFantasia}")
+            print(f"Giro: {cliente.giro}")
+            print(f"DirPart: {cliente.dirPart}")
+            print(f"DirFact: {cliente.dirFact}")
+            print(f"CorreoPar: {cliente.correoPar}")
+            print(f"CorreoFact: {cliente.correoFact}")
+            print(f"Ciudad: {cliente.ciudad}")
+            print(f"Comuna: {cliente.comuna}")
+            print(f"Activo: {cliente.activo}")
+            print("\n")
     except Exception as err:
         print(f"Error: {err}")
     finally:
-        await client_api.Productos.close()
+        await client_api.Clientes.close()
+       
 if __name__ == "__main__":
     asyncio.run(main())
+    
+
+
     
--- SimpleFacturaSDK/services/ProveedorService.py
+++ SimpleFacturaSDK/services/ProveedorService.py
@@ -4,54 +4,77 @@ from models.GetFactura.Dte import Dte
 from Utilidades.Simplificar_error import simplificar_errores
 import requests
 from models.SerializarJson import serializar_solicitud, serializar_solicitud_dict,dataclass_to_dict
-
+import aiohttp
 
 class ProveedorService:
-    def __init__(self, session, base_url):
-        self.session = session
+    def __init__(self, base_url, headers):
         self.base_url = base_url
+        self.headers = headers
+        self.session = aiohttp.ClientSession(headers=self.headers)
 
-    def listarDteRecibidos(self, solicitud) -> Response[Optional[List[Dte]]]:
+    async def listarDteRecibidos(self, solicitud) -> Response[Optional[List[Dte]]]:
         url = f"{self.base_url}/documentsReceived"
         solicitud_dict = serializar_solicitud_dict(solicitud)
-        response = self.session.post(url, json=solicitud_dict)
-        contenidoRespuesta = response.text
-        if response.status_code == 200:
-            deserialized_response = Response[List[Dte]].parse_raw(contenidoRespuesta)
-            return Response(status=200, data=deserialized_response.data)
-        return Response(
-            status=response.status_code,
-            message=simplificar_errores(contenidoRespuesta),
-            data=None
-        )
+        try:
+            async with self.session.post(url, json=solicitud_dict) as response:
+                contenidoRespuesta = await response.text()
+                if response.status == 200:
+                    deserialized_response = Response[List[Dte]].parse_raw(contenidoRespuesta)
+                    return Response(status=200, data=deserialized_response.data)
+                return Response(
+                    status=response.status,
+                    message=simplificar_errores(contenidoRespuesta),
+                    data=None
+                )
+        except aiohttp.ClientError as err:
+            return Response(
+                status=500,
+                message=f"Error en la conexión: {err}",
+                data=None
+            )
 
-    def obtenerXml(self, solicitud) -> Response[bytes]:
+    async def obtenerXml(self, solicitud) -> Response[bytes]:
         url = f"{self.base_url}/documentReceived/xml"
         solicitud_dict = serializar_solicitud_dict(solicitud)
-        response = self.session.post(url, json=solicitud.to_dict())
-        contenidoRespuesta = response.text
-        if response.status_code == 200:
-            return Response(status=200, data=response.content)
-        return Response(
-            status=response.status_code,
-            message=simplificar_errores(contenidoRespuesta),
-            data=None
-        )
-
-    def obtener_pdf(self, solicitud):
+        try:
+            async with self.session.post(url, json=solicitud_dict) as response:
+                contenidoRespuesta = await response.text()
+                if response.status == 200:
+                    return Response(status=200, data=await response.read())
+                return Response(
+                    status=response.status,
+                    message=simplificar_errores(contenidoRespuesta),
+                    data=None
+                )
+        except aiohttp.ClientError as err:
+            return Response(
+                status=500,
+                message=f"Error en la conexión: {err}",
+                data=None
+            )
+    
+    async def obtener_pdf(self, solicitud) -> Response[bytes]:
         url = f"{self.base_url}/documentReceived/getPdf"
         solicitud_dict = serializar_solicitud_dict(solicitud)
-        response = self.session.post(url, json=solicitud_dict)
-        contenidoRespuesta = response.text
-        if response.status_code == 200:
-            return Response(status=200, data=response.content)
-        return Response(
-            status=response.status_code,
-            message=simplificar_errores(contenidoRespuesta),
-            data=None
-        )
+        try:
+            async with self.session.post(url, json=solicitud_dict) as response:
+                contenidoRespuesta = await response.read()
+                if response.status == 200:
+                    return Response(status=200, data=contenidoRespuesta)
+                return Response(
+                    status=response.status,
+                    message=simplificar_errores(contenidoRespuesta),
+                    data=None
+                )
 
-    def ConciliarRecibidos(self, solicitud, mes, anio) -> str:
+        except aiohttp.ClientError as err:
+            return Response(
+                status=500,
+                message=f"Error en la conexión: {err}",
+                data=None
+            )
+
+    async def ConciliarRecibidos(self, solicitud, mes, anio) -> str:
         url = f"{self.base_url}/documentsReceived/consolidate/{mes}/{anio}"
         if not isinstance(mes, int):
             return Response(
@@ -67,13 +90,24 @@ class ProveedorService:
             )
         
         solicitud_dict = serializar_solicitud_dict(solicitud)
-        response = self.session.post(url, json=solicitud_dict)
-        contenidoRespuesta = response.text
-        if response.status_code == 200:
-            deserialize_response = Response[str].parse_raw(contenidoRespuesta)
-            return Response(status=200, data=deserialize_response.data)
-        return Response(
-            status=response.status_code,
-            message=simplificar_errores(contenidoRespuesta),
-            data=None
-        )
+        try:
+            async with self.session.post(url, json=solicitud_dict) as response:
+                contenidoRespuesta = await response.text()
+                if response.status == 200:
+                    deserialize_response = Response[str].parse_raw(contenidoRespuesta)
+                    return Response(status=200, data=deserialize_response.data)
+                return Response(
+                    status=response.status,
+                    message=simplificar_errores(contenidoRespuesta),
+                    data=None
+                )
+        except aiohttp.ClientError as err:
+            return Response(
+                status=500,
+                message=f"Error en la conexión: {err}",
+                data=None
+            )
+
+    async def close(self):
+        if not self.session.closed:
+            await self.session.close()
