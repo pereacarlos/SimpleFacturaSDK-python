97671f24cace5a8e94d8938d489cbdc10ca1d4eb
carlosperea14 <carlos.perea@inacapmail.cl>
2024-11-20 16:59:09 -0300

    se hicieron cambios en clientes

--- SimpleFacturaSDK/main.py
+++ SimpleFacturaSDK/main.py
@@ -1,70 +1,21 @@
 import asyncio
 
 from ClientSimpleFactura import ClientSimpleFactura
-import base64
-import requests
-import json
-from models.ResponseDTE import Response
-from enumeracion.Ambiente import AmbienteEnum
-from enumeracion.TipoDTE import DTEType
 from models.GetFactura.Credenciales import Credenciales
-from models.Productos.DatoExternoRequest import DatoExternoRequest
-from models.Productos.NuevoProductoExternoRequest import NuevoProductoExternoRequest
-from models.Clientes.NuevoReceptorExternoRequest import NuevoReceptorExternoRequest
 
 async def main():
     username = "demo@chilesystems.com"
     password = "Rv8Il4eV"
     client_api = ClientSimpleFactura(username, password)
-    solicitud= DatoExternoRequest(
-        Credenciales=Credenciales(
-            rut_emisor="76269769-6",
-            nombre_sucursal="Casa Matriz"
-        ),
-        Clientes=[
-            NuevoReceptorExternoRequest(
-                Rut="57681892-0",
-                RazonSocial="Cliente Test 1",
-                Giro="Giro 1",
-                DirPart="direccion 1",
-                DirFact="direccion 1",
-                CorreoPar="correo 1",
-                CorreoFact="correo 1",
-                Ciudad="Ciudad 1",
-                Comuna="Comuna 1"
-            ),
-            NuevoReceptorExternoRequest(
-                Rut="56516677-8",
-                RazonSocial="Cliente Test 2",
-                Giro="Giro 2",
-                DirPart="direccion 2",
-                DirFact="direccion 2",
-                CorreoPar="correo 2",
-                CorreoFact="correo 2",
-                Ciudad="Ciudad 2",
-                Comuna="Comuna 2"
-            ),
-            NuevoReceptorExternoRequest(
-                Rut="68959276-7",
-                RazonSocial="Cliente Test 3",
-                Giro="Giro 3",
-                DirPart="direccion 3",
-                DirFact="direccion 3",
-                CorreoPar="correo 3",
-                CorreoFact="correo 3",
-                Ciudad="Ciudad 3",
-                Comuna="Comuna 3"
-            )
-        ]
-    )
+    solicitud= Credenciales(rut_emisor="76269769-6")
+
 
     try:
-        AddClient = await client_api.Clientes.CrearClientes(solicitud)
+        ListClient = await client_api.Clientes.ListarClientes(solicitud)
         print("\nDatos de la Respuesta:")
-        print(f"Status: {AddClient.status}")
-        print(f"Message: {AddClient.message}")
-        
-        for cliente in AddClient.data:
+        print(f"Status: {ListClient.status}")
+        print(f"Message: {ListClient.message}")
+        for cliente in ListClient.data:
             print(f"ReceptorId: {cliente.receptorId}")
             print(f"EmisorId: {cliente.emisorId}")
             print(f"RUT: {cliente.rut}")
@@ -81,6 +32,7 @@ async def main():
             print(f"Comuna: {cliente.comuna}")
             print(f"Activo: {cliente.activo}")
             print("\n")
+
     except Exception as err:
         print(f"Error: {err}")
     finally:
