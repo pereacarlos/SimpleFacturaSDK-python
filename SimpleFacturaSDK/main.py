60c2cf694d767bfb8e6509b1523bf3f1f81494fc
carlosperea14 <carlos.perea@inacapmail.cl>
2024-11-20 23:32:25 -0300

    se hicieron cambios

--- SimpleFacturaSDK/main.py
+++ SimpleFacturaSDK/main.py
@@ -1,30 +1,43 @@
 import asyncio
 
 from ClientSimpleFactura import ClientSimpleFactura
+from models.BoletaHonorarios.ListaBHERequest import ListaBHERequest
 from models.GetFactura.Credenciales import Credenciales
+from datetime import datetime
+fecha_desde = datetime.strptime("2024-09-03", "%Y-%m-%d").isoformat()
+fecha_hasta = datetime.strptime("2024-11-11", "%Y-%m-%d").isoformat()
+
 
 async def main():
     username = "demo@chilesystems.com"
     password = "Rv8Il4eV"
     client_api = ClientSimpleFactura(username, password)
-    solicitud= Credenciales(
-        rut_emisor="76269769-6"
+    solicitud= ListaBHERequest(
+        credenciales=Credenciales(
+            rut_emisor="76269769-6",
+            nombre_sucursal="Casa Matriz"
+        ),
+        Folio=None,
+        Desde=fecha_desde,
+        Hasta=fecha_hasta
     )
 
     try:
-        DatosEmpresa = await client_api.ConfiguracionService.datos_empresa(solicitud)
+        ListadoBHERecibido = await client_api.BoletaHonorarioService.ListadoBHERecibido(solicitud)
         print("\nDatos de la Respuesta:")
-        print(f"Status: {DatosEmpresa.status}")
-        print(f"Message: {DatosEmpresa.message}")
-        print(f"Data: {DatosEmpresa.data}")
-        print(f"rut: {DatosEmpresa.data.rut}")
-        print(f"razonSocial: {DatosEmpresa.data.razonSocial}")
-        print(f"giro: {DatosEmpresa.data.giro}")
-   
+        print(f"Status: {ListadoBHERecibido.status}")
+        print(f"Message: {ListadoBHERecibido.message}")
+        for cliente in ListadoBHERecibido.data:
+            print(f"fOLIO: {cliente.folio}")
+            print(f"FECHAEMISION: {cliente.fechaEmision}")
+            print("\n")     
+
+
     except Exception as err:
         print(f"Error: {err}")
     finally:
-        await client_api.ConfiguracionService.close()
+        await client_api.BoletaHonorarioService.close()
+    
        
 if __name__ == "__main__":
     asyncio.run(main())
