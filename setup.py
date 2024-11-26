9bec709b4a6562409af1cbb6076771f29dc8f8ce
carlosperea14 <carlos.perea@inacapmail.cl>
2024-11-26 00:14:21 -0300

    se hicieron cambios

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SimpleFacturaSDK",
-    version="1.0",
+    version="1.0.1",
     description="SDK para la integración con los servicios de SimpleFactura",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
