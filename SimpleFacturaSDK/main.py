edcf3e46aaa93465073b4c6e6cdf46c1bb76d3d3
Carlosperea1 <cperea@chilesystems.com>
2026-05-28 16:41:38 -0400

    s

--- SimpleFacturaSDK/main.py
+++ SimpleFacturaSDK/main.py
@@ -1,34 +1,33 @@
-from SimpleFacturaSDK.client_simple_factura import ClientSimpleFactura
-from SimpleFacturaSDK.enumeracion.SyncTypeEnum import SyncTypeEnum
-from SimpleFacturaSDK.models.GetFactura.UltimaSyncRequest import UltimaSyncRequest
-from SimpleFacturaSDK.models.GetFactura.Credenciales import Credenciales
 import asyncio
-import httpx
 import os
+
 from dotenv import load_dotenv
+
+from SimpleFacturaSDK.client_simple_factura import ClientSimpleFactura
+from SimpleFacturaSDK.models.GetFactura.Credenciales import Credenciales
+from SimpleFacturaSDK.models.Payku.PaykuRequests import PaykuToggleRequest
+
 load_dotenv()
 username = os.getenv("SF_USERNAME")
 password = os.getenv("SF_PASSWORD")
+
+
 async def main():
     async with ClientSimpleFactura(username, password) as client_api:
-        solicitud = UltimaSyncRequest(
-            Credenciales=Credenciales(
-                rut_emisor="78181331-1"
-            ),
-            Tipo=SyncTypeEnum.Ventas
+        solicitud = PaykuToggleRequest(
+            credenciales=Credenciales(rut_emisor="78181331-1"),
+            activo=True,
         )
-
         try:
-            ultimaSync = await client_api.Facturacion.ultima_sincronizacion(solicitud, mes=10, anio=2025)
+            respuesta = await client_api.PaykuService.activar_desactivar(solicitud)
             print("\nDatos de la Respuesta:")
-            print(f"Status: {ultimaSync.status}")
-            print(f"Message: {ultimaSync.message}")
-            print(f"Data: {ultimaSync.data}")
-            print(f"Errors: {ultimaSync.errors}")
-        except httpx.HTTPStatusError as err:
-            print(f"Error: {err}")
+            print(f"Status: {respuesta.status}")
+            print(f"Message: {respuesta.message}")
+            print(f"Data: {respuesta.data}")
+            print(f"Errors: {respuesta.errors}")
         except Exception as err:
             print(f"Error: {err}")
-       
+
+
 if __name__ == "__main__":
     asyncio.run(main())
